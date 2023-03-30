b1e77a736911c0fea36019ccdd9f288a61c4f61f
jlwetler <joaolucasmw@gmail.com>
2023-03-29 23:57:55 -0300

    fix orm problems

--- src/services/signUpService.ts
+++ src/services/signUpService.ts
@@ -1,20 +1,31 @@
 import bcrypt from 'bcrypt';
+import { getRepository } from "typeorm";
+import User from "../entities/User"
 
-interface User {
+interface UserObject {
     name: string, 
-    lastName: string, 
     email: string, 
     password: string
 };
 
-export async function authenticateSignUp(userObject: User) {
-    const { name, lastName, email, password } = userObject;
+export async function authenticateSignUp(userObject: UserObject) {
+    const { name, email, password } = userObject;
 
     const hashPassword = bcrypt.hashSync(password, 12);
 
+    const check = await getRepository(User).insert({
+        name
+    })
+    
     //const check = await emailRepository.emailCheck(email);
 
     //if (check === null) return null;
     
+    await getRepository(User).findOne({
+        where: {
+            name, email, password: hashPassword
+          }
+    })
+
    // await userRepository.insertUserData(name, lastName, email, hashPassword);
 }
